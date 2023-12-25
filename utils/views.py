dceb69b1f3d50ec0c8410ee86a9f243a2265bfc2
Dmitriy <6468831@mail.ru>
2023-12-25 21:12:19 +0300

    debugging

--- utils/views.py
+++ utils/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
 from badshop_django.logger import logger
 
+
+@method_decorator(csrf_exempt, name='dispatch')
 def yoo_kassa_webhook_view(request):
     logger.debug(f'yoo_kassa request data = {request.POST}')
     return HttpResponse('')
