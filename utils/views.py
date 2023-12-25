fc43d183f6f023c54908b149503f6fffe279b719
Dmitriy <6468831@mail.ru>
2023-12-25 21:21:49 +0300

    debugging

--- utils/views.py
+++ utils/views.py
@@ -4,9 +4,20 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
 from badshop_django.logger import logger
+from yookassa import Configuration, Webhook
+from yookassa import Payment as yoo_Payment
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 def yoo_kassa_webhook_view(request):
-    logger.debug(f'yoo_kassa request data = {request.POST}')
+    Configuration.account_id = '285619'
+    Configuration.secret_key = 'test_pehJPGfr6C3c-BqjXCg7CzYq5PsIDdGjBxu0hwRQGxY'
+
+
+    response = Webhook.add({
+        "event": "payment.succeeded",
+        "url": "https://vitanow.ru/test_yoo_kassa_response/",
+    })
+    logger.debug(f'yoo_kassa SDK data = {response}')
+    logger.debug(f'yoo_kassa request data = {request.GET}')
     return HttpResponse('')
