f2f2c36c04de3c1ce16ce1496271b689e9bbdfeb
unknown <6468831@mail.ru>
2024-01-29 10:57:03 +0300

    debugging filters

--- index/services.py
+++ index/services.py
@@ -3,6 +3,7 @@ from django.contrib.sessions.models import Session
 from django.db.models import Q
 
 from items.models import Item, Cart
+from badshop_django.logger import logger
 
 def get_filter_items(max_item_price, query, brend, category, bad, price_max, price_min):
 
@@ -16,12 +17,12 @@ def get_filter_items(max_item_price, query, brend, category, bad, price_max, pri
         
     if bad and bad != ['']:
         filter_dict['active_bad__id'] = bad
-        
+    
     if query:
         q_objects |= Q(name__icontains=query)
         q_objects |= Q(brend__name__icontains=query)
         q_objects |= Q(category__title__icontains=query)
-        q_objects |= Q(bad__title__icontains=query)
+        q_objects |= Q(active_bad__title__icontains=query)
     
     items = Item.objects.filter(
         q_objects,
