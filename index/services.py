cd6f99228362ed9309643ae4c3c1f8e85cf460b3
unknown <6468831@mail.ru>
2024-02-01 11:42:52 +0300

    debugging

--- index/services.py
+++ index/services.py
@@ -6,7 +6,7 @@ from items.models import Item, Cart
 from badshop_django.logger import logger
 
 def get_filter_items(max_item_price, query, brend, category, bad, price_max, price_min):
-
+    logger.debug(f'category in get_filter_items = {category}')
     filter_dict = {}
     q_objects = Q()
     if category:
@@ -14,7 +14,8 @@ def get_filter_items(max_item_price, query, brend, category, bad, price_max, pri
 
     if brend and brend != ['']:
         filter_dict['brend__id__in'] = brend
-        
+    
+    # active_substance  
     if bad and bad != ['']:
         filter_dict['active_bad__id'] = bad
     
