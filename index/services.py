a1bd720f74eb0d27a09958e0c017b2e7bd45f281
ARTEM BOYKO <boyko.a.v@bk.ru>
2024-02-08 19:59:37 +0700

    active bads

--- index/services.py
+++ index/services.py
@@ -6,23 +6,29 @@ from items.models import Item, Cart
 from badshop_django.logger import logger
 
 def get_filter_items(max_item_price, query, brend, category, bad, price_max, price_min):
+
     filter_dict = {}
     q_objects = Q()
+
     if category:
         filter_dict['category__id'] = category
     if brend and brend != ['']:
         filter_dict['brend__id__in'] = brend
+
+    # active_substance  
     if bad and bad != ['']:
-        q_objects |= Q(active_bads__title__icontains=bad)
+        filter_dict['active_bads__title__icontains'] = query
     if query:
         q_objects |= Q(name__icontains=query)
         q_objects |= Q(brend__name__icontains=query)
         q_objects |= Q(category__title__icontains=query)
         q_objects |= Q(vendor_code__icontains=query)
+        q_objects |= Q(active_bads__title__icontains=query)
     items = Item.objects.filter(
         q_objects,
         price__range=(price_min, price_max),
         **filter_dict
     )
+
     return items
 
