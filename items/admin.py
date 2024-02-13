67898739243dee0875e0590383e30d9880306bfd
unknown <6468831@mail.ru>
2024-02-13 12:40:41 +0300

    bugfixes

--- items/admin.py
+++ items/admin.py
@@ -1,65 +1,3 @@
-<<<<<<< HEAD
-from django.contrib import admin
-from django.db import models
-
-from items.models import (
-    Item, Category, Brend, 
-    CertificateImages, CartItem, 
-    Cart, FavoriteItem, ItemImages, 
-    UserItem, ActiveBad, Review
-)
-
-
-class CertificateImageInline(admin.TabularInline):  # или `admin.TabularInline` для табличного представления
-    model = CertificateImages
-    extra = 1
-# Register your models here.
-
-class ItemImagesInline(admin.TabularInline):
-    model = ItemImages
-    extra = 1
-
-@admin.register(Item)
-class ItemAdmin(admin.ModelAdmin):
-    inlines = (CertificateImageInline, ItemImagesInline)
-    list_display = ('name', 'price', 'discount', 'bonus_percentage','views_count', 'category') # 'sale_price',
-    list_filter = ('name', 'price')
-    search_fields = ('name', 'price')
-    prepopulated_fields = {"slug": ("name",)}
-
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('title',)
-    list_filter = ('title',)
-    list_editable = ('title',)
-    search_fields = ('title',)
-
-@admin.register(FavoriteItem)
-class FavoriteItemAdmin(admin.ModelAdmin):
-    list_display = ('item', 'user', 'session', 'date_added')
-
-@admin.register(Cart)
-class CartAdmin(admin.ModelAdmin):
-    list_display = ('user', 'session',)
-
-@admin.register(CartItem)
-class CartItemAdmin(admin.ModelAdmin):
-    list_display = ('item', 'quantity', 'cart', 'date_added')
-
-
-@admin.register(UserItem)
-class UserItemAdmin(admin.ModelAdmin):
-    list_display = ('user', 'item')
-
-
-@admin.register(Review)
-class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('text',)
-
-admin.site.register(Category)
-admin.site.register(CertificateImages)
-admin.site.register(Brend)
-admin.site.register(ActiveBad)
-=======
 from django.contrib import admin
 from django.db import models
 
@@ -116,4 +54,3 @@ admin.site.register(Category)
 admin.site.register(CertificateImages)
 admin.site.register(Brend)
 admin.site.register(ActiveBad)
->>>>>>> 1203ec5 (item, category slug, fixed duplicated buttons)
--- utils/services.py
+++ utils/services.py
@@ -1,61 +1,3 @@
-<<<<<<< HEAD
-from django.contrib.sessions.models import Session
-from django.contrib.auth.forms import PasswordResetForm
-from django.http import HttpRequest
-
-from PIL import Image as pil_image
-
-
-def save_model_image(self, size, field):
-
-    image_field = self.__getattribute__(field)
-
-    img = pil_image.open(image_field.path)
-
-    x = img.width
-    y = img.height
-    ratio = x/y
-    min_dimension = size
-
-    if x > y:
-        output_size = (min_dimension * ratio, min_dimension)
-    else:
-        output_size = (min_dimension, min_dimension * ratio)
-    img.thumbnail(output_size)
-    img.save(image_field.path)
-
-
-
-def get_current_session(request):
-    session = Session.objects.get(
-        session_key=request.session.session_key
-        )
-    return session
-
-
-def create_user_or_session_filter_dict(request):
-    filter_dict = {}
-
-    if request.user.is_authenticated:
-        filter_dict['user'] = request.user
-    elif request.session.session_key != None:
-        session = get_current_session(request)
-        filter_dict['session'] = session
-    return filter_dict
-
-
-def password_reset_for_new_user(request, email):
-    form = PasswordResetForm({'email': email})
-    if form.is_valid():
-        request = HttpRequest()
-        request.META['SERVER_NAME'] = 'https://vitanow.ru'
-        request.META['SERVER_PORT'] = '443'
-        form.save(
-            request=request,
-            use_https=True,
-            from_email="no-reply@vitanow.ru", 
-            email_template_name='registration/password_reset_email.html')
-=======
 import uuid
 
 from slugify import slugify
@@ -133,4 +75,3 @@ def unique_slug_generator(obj, new_slug=None):
         return unique_slug_generator(new_slug=new_slug)
 
     obj.slug = slug
->>>>>>> 1203ec5 (item, category slug, fixed duplicated buttons)
